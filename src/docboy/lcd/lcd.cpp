3b6010e3c6f320cf90ab7a8728e71d9efc51a4a2
Docheinstein <dottorestefano97@gmail.com>
2023-10-17 21:42:07 +0200

    LCD: serialize also framebuffer when save state

--- src/docboy/lcd/lcd.cpp
+++ src/docboy/lcd/lcd.cpp
@@ -2,12 +2,14 @@
 #include "utils/parcel.h"
 
 void Lcd::saveState(Parcel& parcel) const {
+    parcel.writeBytes(pixels, sizeof(pixels));
     parcel.writeUInt16(cursor);
     DEBUGGER_ONLY(parcel.writeUInt8(x));
     DEBUGGER_ONLY(parcel.writeUInt8(y));
 }
 
 void Lcd::loadState(Parcel& parcel) {
+    parcel.readBytes(pixels, sizeof(pixels));
     cursor = parcel.readUInt16();
     DEBUGGER_ONLY(x = parcel.readUInt8());
     DEBUGGER_ONLY(y = parcel.readUInt8());
