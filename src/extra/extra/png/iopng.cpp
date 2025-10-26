172b42da39151be7354d8ff71a0bf9239224bdaa
Docheinstein <dottorestefano97@gmail.com>
2025-10-26 22:13:51 +0100

    Build: fix bug caused by conflicts between static and dynamic libpng

--- src/extra/extra/png/iopng.cpp
+++ src/extra/extra/png/iopng.cpp
@@ -16,12 +16,12 @@ bool save_png_rgb888(const std::string& filename, const void* buffer, uint32_t w
 
     bool success {};
 
-    png = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+    png = docboy_png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
     if (!png) {
         goto error;
     }
 
-    info = png_create_info_struct(png);
+    info = docboy_png_create_info_struct(png);
     if (!info) {
         goto error;
     }
@@ -35,11 +35,11 @@ bool save_png_rgb888(const std::string& filename, const void* buffer, uint32_t w
         goto error;
     }
 
-    png_init_io(png, fp);
+    docboy_png_init_io(png, fp);
 
-    png_set_IHDR(png, info, width, height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT,
-                 PNG_FILTER_TYPE_DEFAULT);
-    png_write_info(png, info);
+    docboy_png_set_IHDR(png, info, width, height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+                        PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    docboy_png_write_info(png, info);
 
     raw_pointers = new png_byte*[height];
 
@@ -47,8 +47,8 @@ bool save_png_rgb888(const std::string& filename, const void* buffer, uint32_t w
         raw_pointers[y] = const_cast<unsigned char*>(static_cast<const unsigned char*>(buffer)) + y * width * 3;
     }
 
-    png_write_image(png, raw_pointers);
-    png_write_end(png, nullptr);
+    docboy_png_write_image(png, raw_pointers);
+    docboy_png_write_end(png, nullptr);
 
     success = true;
 
@@ -57,7 +57,7 @@ error:
         fclose(fp);
     }
 
-    png_destroy_write_struct(&png, &info);
+    docboy_png_destroy_write_struct(&png, &info);
 
     delete[] raw_pointers;
 
@@ -87,12 +87,12 @@ std::vector<uint8_t> load_png_rgb888(const std::string& filename, uint32_t* widt
         goto error;
     }
 
-    png = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+    png = docboy_png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
     if (!png) {
         goto error;
     }
 
-    info = png_create_info_struct(png);
+    info = docboy_png_create_info_struct(png);
     if (!info) {
         goto error;
     }
@@ -101,41 +101,41 @@ std::vector<uint8_t> load_png_rgb888(const std::string& filename, uint32_t* widt
         goto error;
     }
 
-    png_init_io(png, fp);
+    docboy_png_init_io(png, fp);
 
-    png_read_info(png, info);
+    docboy_png_read_info(png, info);
 
-    width = png_get_image_width(png, info);
-    height = png_get_image_height(png, info);
-    color_type = png_get_color_type(png, info);
-    bit_depth = png_get_bit_depth(png, info);
+    width = docboy_png_get_image_width(png, info);
+    height = docboy_png_get_image_height(png, info);
+    color_type = docboy_png_get_color_type(png, info);
+    bit_depth = docboy_png_get_bit_depth(png, info);
 
     if (bit_depth == 16) {
-        png_set_strip_16(png);
+        docboy_png_set_strip_16(png);
     }
 
     switch (color_type) {
     case PNG_COLOR_TYPE_PALETTE:
-        png_set_palette_to_rgb(png);
+        docboy_png_set_palette_to_rgb(png);
         break;
     case PNG_COLOR_TYPE_GRAY:
     case PNG_COLOR_TYPE_GRAY_ALPHA:
-        png_set_gray_to_rgb(png);
+        docboy_png_set_gray_to_rgb(png);
     case PNG_COLOR_TYPE_RGB_ALPHA:
-        png_set_strip_alpha(png);
+        docboy_png_set_strip_alpha(png);
     }
 
-    png_read_update_info(png, info);
+    docboy_png_read_update_info(png, info);
 
     raw_pointers = new png_byte*[height];
 
-    bytes_per_row = png_get_rowbytes(png, info);
+    bytes_per_row = docboy_png_get_rowbytes(png, info);
 
     for (uint32_t y = 0; y < height; y++) {
         raw_pointers[y] = new png_byte[bytes_per_row];
     }
 
-    png_read_image(png, raw_pointers);
+    docboy_png_read_image(png, raw_pointers);
 
     result.resize(height * bytes_per_row);
 
@@ -157,7 +157,7 @@ error:
         fclose(fp);
     }
 
-    png_destroy_read_struct(&png, &info, nullptr);
+    docboy_png_destroy_read_struct(&png, &info, nullptr);
 
     if (raw_pointers) {
         for (uint32_t y = 0; y < height; y++) {
