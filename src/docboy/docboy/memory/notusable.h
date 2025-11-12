c9b0bb115a5bb8f7b4cc72c95337825f67579fed
Docheinstein <dottorestefano97@gmail.com>
2025-11-12 18:40:31 +0100

    DMG mode: add tests for not usable area in DMG mode

--- src/docboy/docboy/memory/notusable.h
+++ src/docboy/docboy/memory/notusable.h
@@ -1,6 +1,7 @@
 #ifndef NOTUSABLE_H
 #define NOTUSABLE_H
 
+#include "docboy/common/specs.h"
 #include "docboy/memory/memory.h"
 
 class NotUsable : public Memory<Specs::MemoryLayout::NOT_USABLE::START, Specs::MemoryLayout::NOT_USABLE::END> {};
